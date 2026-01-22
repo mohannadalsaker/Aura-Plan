8e19f37338d9a90a502a82802ad82b7b9c090be8
mohannadalsaker <mhndsaker@gmail.com>
2026-01-22 18:01:37 +0300

    downgrading prisma to v6

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,19 +1,11 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { env } from '@prisma/config';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    const adapter = new PrismaPg({
-      connectionString: env('DATABASE_URL'),
-    });
-    super({ adapter });
-  }
   async onModuleInit() {
     await this.$connect();
   }
